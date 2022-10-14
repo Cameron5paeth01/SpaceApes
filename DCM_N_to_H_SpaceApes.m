c36910409db511824dd59aa24a6257951875ce06
Cameron Spaeth <98167080+Cameron5paeth01@users.noreply.github.com>
2022-10-14 13:57:12 -0400

    Update DCM_N_to_H_SpaceApes.m

--- DCM_N_to_H_SpaceApes.m
+++ DCM_N_to_H_SpaceApes.m
@@ -1,10 +1,10 @@
 %
 function [R_N_to_H, omega_H_rel_N_as_N] = DCM_N_to_H_SpaceApes(r_N, r_dot_N,n)
-omega_H_rel_N_as_N = n*[0;0;1];
 h_r = r_N/norm(r_N);
 h_h = (skew(r_N)*r_dot_N)/norm(skew(r_N)*r_dot_N);
 h_theta = skew(h_h)*h_r;
 N = eye(3);
 H = [h_r';h_theta';h_h'];
 R_N_to_H = H*N;
+omega_H_rel_N_as_N = R_N_to_H'*n*[0;0;1];
 end
