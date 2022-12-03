2587cb861994605655d162cc544b83b1223d22f4
nrs0025 <115818956+nrs0025@users.noreply.github.com>
2022-12-03 14:34:49 -0500

    Add files via upload

---
+++ SpaceApes_Task4.m
@@ -0,0 +1,29 @@
+%% Development Information
+% MAE 466 Spacecraft Dynamics
+% Space Apes Project Development
+% 
+%
+% 
+% Primary Developer Contact Information:
+% Cameron Spaeth
+% Undergraduate Student
+% Statler College of Engineering & Mineral Resources
+% Dept. Mechanical and Aerospace Engineering
+% West Virginia University (WVU)
+% cds0060@mix.wvu.edu
+%
+% Noah Sampson
+% Undergraduate Student
+% Statler College of Engineering & Mineral Resources
+% Dept. Mechanical and Aerospace Engineering
+% West Virginia University (WVU)
+% nrs0025@mix.wvu.edu
+%
+%
+% Development History
+% Date              Developer        Comments
+% ---------------   -------------    --------------------------------
+% Oct. 2022         Noah S.       
+%                         Cameron S.
+% DCM is constant, just call function
+[R_N_to_Rs, omega_Rs_rel_N] = DCM_N_to_Rs_SpaceApes
